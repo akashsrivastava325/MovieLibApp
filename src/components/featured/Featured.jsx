085f42702a0c2f0b922f8db2495145f74778001c
akashsrivastava325 <akashsrivastava479@gmail.com>
2023-02-22 22:53:22 +0530

    deploying movieLib app version 1.1 deployed v1.1
    wq
    
    :wq

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,51 @@
+import { InfoOutlined, PlayArrow } from "@material-ui/icons";
+import "./featured.scss";
+import mainCover from "../images/mainCover.jpg";
+
+export default function Featured({ type }) {
+  return (
+    <div className="featured">
+      {type && (
+        <div className="category">
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select name="genre" id="genre">
+            <option>Genre</option>
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="crime">Crime</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="historical">Historical</option>
+            <option value="horror">Horror</option>
+            <option value="romance">Romance</option>
+            <option value="sci-fi">Sci-fi</option>
+            <option value="thriller">Thriller</option>
+            <option value="western">Western</option>
+            <option value="animation">Animation</option>
+            <option value="drama">Drama</option>
+            <option value="documentary">Documentary</option>
+          </select>
+        </div>
+      )}
+      <img src={mainCover} alt="" />
+      <div className="info">
+        <span className="desc">
+          Dark is a German science fiction thriller television series co-created
+          by Baran bo Odar and Jantje Friese. It ran for three seasons from 2017
+          to 2020. The story follows characters from the fictional town of
+          Winden, Germany, as they pursue the truth in the aftermath of a
+          child's disappearance.
+        </span>
+        <div className="buttons">
+          <button className="play">
+            <PlayArrow />
+            <span>Play</span>
+          </button>
+          <button className="more">
+            <InfoOutlined />
+            <span>Info</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
